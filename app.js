ad11ea1facc7f9ab9c52a1a29ddf9615dbb690c4
Welton Beck <weltonbeck@Weltons-iMac.local>
2016-11-08 16:30:52 -0200

    por canal

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 // Iniciamos uma nova instância do socket.io passando o objeto http(o servidor http)
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use('/static', express.static(__dirname + '/static'));
+
 app.get('/', function(req, res){
-	res.sendfile('index.html');
+	res.sendFile( __dirname + '/index.html');
+});
+
+app.get('/chat', function(req, res){
+	res.sendFile( __dirname + '/chat.html');
 });
 
 // Quando tivermos uma chamada para o nosso socket iremos logar no terminal uma mensagem de novo usuário
 io.on('connection', function(socket){
-	console.log('Usuário conectado.');
-	io.emit('mensagem de chat', 'Usuário conectado.');
 
-	socket.on('mensagem de chat', function(msg){
-	   console.log('nova mensagem enviada: ' + msg);
-
-	   // envia msg só para o usuario que solicitou
-	   // socket.emit('mensagem de chat', 'Nova mensagem enviada');
-
-	   // mensagem para todos os usuarios
-	   io.emit('mensagem de chat', msg);
+	// defino qual a sala dele
+	socket.on('room', function(room) {
+		socket.join(room);
+		io.sockets.in(room).emit('receiver_chat_message', 'Usuário entrou na sala.');
 	});
 
-	socket.on('disconnect', function(){
-		console.log('O usuário saiu da aplicação.');
-		io.emit('mensagem de chat', 'O usuário saiu da aplicação.');
+	socket.on('send_chat_mensseger', function( msg ){
+		// envia msg só para o usuario que solicitou
+		// socket.emit('receiver_chat_message', 'Nova mensagem enviada');
+
+		// mensagem para todos os usuarios
+		room = getUserRoom(socket);	
+		if ( room ) {
+			io.sockets.in(room).emit('receiver_chat_message', msg);
+		}
+	});
+
+	socket.on('disconnecting', function( reason ){
+		room = getUserRoom(socket);	
+		if ( room ) {
+			io.sockets.in(room).emit('receiver_chat_message', 'Usuário saiu da sala.');
+		}
 	});
 	
 });
+
+function getUserRoom (socket) {
+	for(var index in socket.rooms) { 
+		if(socket.rooms[index] != socket.id){
+			return socket.rooms[index];
+		}
+	};
+	return null;
+}
 
 http.listen(3000, function(){
 	console.log('listening on http://localhost:3000');
